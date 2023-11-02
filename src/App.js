688f5244d03449a4df4d878bf0eeff8e1cf58c72
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-11-02 15:54:17 -0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,76 +1,52 @@
 import { useState } from 'react';
-
 import './App.css';
-import Square from './components/Square';
+
+import Board from './components/Board';
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const turn = currentMove % 2 === 0
+  const currentBoard = history[currentMove];
 
 
-export default function Board() {
-  const [turn, setTurn] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  //State 
-  // values, turn, win condition, boxes
+  const handlePlay = (squares) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), squares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
 
-  const handleClick = (index) => {
-    const nextSquares = squares.slice();
-    if (nextSquares[index] || calculateWinner(squares)) return;
-    if (turn) {
-      nextSquares[index] = "X";
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = 'Go to move #' + move;
     } else {
-      nextSquares[index] = "O";
-    };
-    setSquares(nextSquares);
-    setTurn(!turn);
-  }
-
-  const calculateWinner = (square) => {
-    const winConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-
-    for (let i = 0; i < winConditions.length; i++) {
-      const [a, b, c] = winConditions[i];
-      if (square[a] && square[a] === square[b] && square[a] === square[c]) return square[a];
+      description = 'Go to game start';
     }
-    return null;
-  }
 
-  const winner = calculateWinner(squares);
-  let status;
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
 
-  if (winner) {
-    status = "Winner" + winner;
-  } else {
-    status = "Next player: " + (turn ? "X" : "O");
-  }
+  console.log(history)
 
   return (
     <main>
-      <h1>Tic-Tac-Toe</h1>
-      <section className='board'>
-        <div>{status}</div>
-        <div className='board-row'>
-          <Square value={squares[0]} handleClick={() => handleClick(0)} />
-          <Square value={squares[1]} handleClick={() => handleClick(1)} />
-          <Square value={squares[2]} handleClick={() => handleClick(2)} />
-        </div>
-        <div className='board-row'>
-          <Square value={squares[3]} handleClick={() => handleClick(3)} />
-          <Square value={squares[4]} handleClick={() => handleClick(4)} />
-          <Square value={squares[5]} handleClick={() => handleClick(5)} />
-        </div>
-        <div className='board-row'>
-          <Square value={squares[6]} handleClick={() => handleClick(6)} />
-          <Square value={squares[7]} handleClick={() => handleClick(7)} />
-          <Square value={squares[8]} handleClick={() => handleClick(8)} />
-        </div>
+      <Board turn={turn} squares={currentBoard} onPlay={handlePlay} />
+      <section className='game-info'>
+        <ol>
+          {moves}
+        </ol>
       </section>
+
     </main>
   );
 }
